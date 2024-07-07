f000395ec96c88abf00f92f2d2de00de826601f7
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-07 18:32:54 +0200

    Grouping the links

---
+++ app/MainLinks/mainLinks.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherLink from "../WeatherLink/weatherLink";
+import NewsLink from "../NewsLink/newsLink";
+import CultureLink from "../CultureLink/cultureLink";
+
+export default function MainLinks() {
+  return (
+    <div className="flex flex-row justify-end gap-28 mr-60">
+      <WeatherLink /> <NewsLink /> <CultureLink />
+    </div>
+  );
+}
