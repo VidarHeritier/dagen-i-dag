8ae44484ce082c848a3a7cbf3871d83e9573194a
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-07 18:33:28 +0200

    the main scaffolding

---
+++ app/Sceleton/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Sceleton() {
+  return (
+    <main className="mx-8 bg-red-400 px-8 rounded-md ">
+      <h2>Her skjer det ting</h2>
+    </main>
+  );
+}
