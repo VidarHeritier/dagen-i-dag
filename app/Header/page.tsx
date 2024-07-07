a827bc8c95b69d7e6fba59d90f087e528f32a393
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-07 18:32:39 +0200

    another link

---
+++ app/Header/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex justify-center bg-slate-600 mb-8">
+      <h1 className="my-6 text-2xl">Dagen i Dag</h1>
+    </header>
+  );
+}
