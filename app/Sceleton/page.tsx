81f0055a8b25f1dff47dac2103bc90770e0efc22
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-08 04:13:42 +0200

    moved placement

--- app/Sceleton/page.tsx
+++ app/Sceleton/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Sceleton() {
   return (
-    <main className="mx-8 bg-red-400 px-8 rounded-md ">
+    <main className="mx-28 bg-red-400 px-8 rounded-md ">
       <h2>Her skjer det ting</h2>
     </main>
   );
