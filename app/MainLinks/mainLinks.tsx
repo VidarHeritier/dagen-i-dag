57e93676a87ec871f91618dd0128353e5a857f24
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-08 04:13:06 +0200

    moved placement

--- app/MainLinks/mainLinks.tsx
+++ app/MainLinks/mainLinks.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import WeatherLink from "../WeatherLink/weatherLink";
-import NewsLink from "../NewsLink/newsLink";
-import CultureLink from "../CultureLink/cultureLink";
+import WeatherLink from "./WeatherLink/weatherLink";
+import NewsLink from "./NewsLink/newsLink";
+import CultureLink from "./CultureLink/cultureLink";
+import TrafficLink from "./TrafficLink/trafficLink";
 
 export default function MainLinks() {
   return (
     <div className="flex flex-row justify-end gap-28 mr-60">
-      <WeatherLink /> <NewsLink /> <CultureLink />
+      <WeatherLink /> <NewsLink /> <CultureLink /> <TrafficLink />
     </div>
   );
 }
