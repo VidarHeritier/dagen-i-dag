b62af3c9e21f071ac35690f5227d56a509343e04
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 09:55:26 +0200

    Implemented links to pages

--- app/Sceleton/page.tsx
+++ app/Sceleton/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function Sceleton() {
-  return (
-    <main className="mx-28 bg-red-400 px-8 rounded-md ">
-      <h2>Her skjer det ting</h2>
-    </main>
-  );
+interface SkeletonProps {
+  content: React.ReactNode;
 }
+
+const Skeleton: React.FC<SkeletonProps> = ({ content }) => {
+  return (
+    <div className="mx-28 bg-red-400 px-8 rounded-md border-red-600 ">
+      {content}
+    </div>
+  );
+};
+
+export default Skeleton;
