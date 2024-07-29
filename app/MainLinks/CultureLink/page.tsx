cc797e0ca369ced74c533ada79675cbf610d872a
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 09:54:41 +0200

    Implemented links to pages

---
+++ app/MainLinks/CultureLink/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StdButton } from "@/app/ButtonLink/page";
+
+interface CultureLinkProps {
+  onClick?: () => void;
+}
+
+const CultureLink: React.FC<CultureLinkProps> = ({ onClick }) => {
+  return (
+    <div>
+      {}
+      <StdButton>Opplevelser</StdButton>
+    </div>
+  );
+};
+
+export default CultureLink;
