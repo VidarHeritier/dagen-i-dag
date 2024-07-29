379fd21ba1fe043b9928cd68a1ae8ed299af6345
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 09:54:24 +0200

    Implemented links to pages

---
+++ app/Hamburger/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HamburgerMenu() {
+  return (
+    <ol className="flex-col mr-20">
+      <li>-</li>
+      <li>-</li>
+      <li>-</li>
+    </ol>
+  );
+}
