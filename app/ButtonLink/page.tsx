a3f6e5966adce06372c7d1bc3dfec2deb8dbe414
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 13:48:10 +0200

    added component

--- app/ButtonLink/page.tsx
+++ app/ButtonLink/page.tsx
@@ -13,7 +13,7 @@ export const StdButton: React.FC<StdButtonProps> = ({
 }) => {
   return (
     <button
-      className="bg-red-400 p-2 rounded-t-md px-6 border-t-red-600 border-l-red-600 border-r-red-600 border-b-red-400 border-4 hover:bg-red-300 "
+      className="bg-red-400 p-2 rounded-t-md px-6 border-t-red-700 border-l-red-700 border-r-red-500 border-b-red-300 border-4 hover:bg-red-600 hover:scale-110 active:bg-red-500"
       onClick={onClick}
     >
       {children}
