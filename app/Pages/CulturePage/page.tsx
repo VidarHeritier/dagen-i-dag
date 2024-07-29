ccbd90ad52aadce37dac9ac9364962bf7ac8f41e
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 11:03:23 +0200

    Made component for page header

--- app/Pages/CulturePage/page.tsx
+++ app/Pages/CulturePage/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Heading from "../Heading/page";
 export default function CulturePage() {
-  return <p>Opplevelser</p>;
+  return <Heading>Opplevelser</Heading>;
 }
