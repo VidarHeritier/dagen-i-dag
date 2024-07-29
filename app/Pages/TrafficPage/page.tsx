c3e59527015c959740e76ff9e2299f3a401f7472
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-29 11:03:36 +0200

    Made component for page header

--- app/Pages/TrafficPage/page.tsx
+++ app/Pages/TrafficPage/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Heading from "../Heading/page";
 export default function TrafficPage() {
-  return <p>Trafikken direkte</p>;
+  return <Heading>Trafikken direkte</Heading>;
 }
