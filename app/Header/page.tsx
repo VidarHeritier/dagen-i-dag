96b53f265a06a5970ae72d2ef3e4f96b80a106a7
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-07-30 11:52:03 +0200

    small tweak

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="flex justify-between bg-slate-600 mb-8">
       <p className="ml-20">´</p>
-      <h1 className="my-6 text-2xl">Dagen i Dag</h1>
+      <h1 className="my-6 text-3xl">Dagen i Dag</h1>
       <HamburgerMenu />
     </header>
   );
