cd9dac87331106bbb70732d5c210b5b97aaa6629
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-08-23 09:17:48 +0200

    new

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -4,7 +4,7 @@ import HamburgerMenu from "../Hamburger/page";
 export default function Header() {
   return (
     <header className="flex justify-between bg-slate-600 mb-8">
-      <p className="ml-20">´</p>
+      <p className="ml-20"> </p>
       <h1 className="my-6 text-3xl">Dagen i Dag</h1>
       <HamburgerMenu />
     </header>
