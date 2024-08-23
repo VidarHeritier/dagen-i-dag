af338431a782651843c1f8dc859400eff2e6da29
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2024-08-23 09:17:32 +0200

    new

--- app/Location/Computer/page.tsx
+++ app/Location/Computer/page.tsx
@@ -1,28 +1,73 @@
-import { useState } from "react";
+"use client";
+
+import { useState, useCallback } from "react";
+import axios from "axios";
 
 const useBrowserGeolocation = () => {
   const [location, setLocation] = useState<{
-    latitude: number;
-    longitude: number;
+    city: string;
+    country: string;
   } | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
-        },
-        (err) => {
-          setError("Error getting location");
-        }
-      );
-    } else {
-      setError("Geolocation is not supported by this browser.");
+  const getLocation = useCallback(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      return;
     }
-  };
 
-  return { location, error, getLocation };
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        convertLatLongToCityCountry(latitude, longitude)
+          .then((loc) => {
+            setLocation(loc);
+            setError(null);
+          })
+          .catch((err) => {
+            setError("Error converting coordinates to location");
+          });
+      },
+      (err) => {
+        setError(`Error fetching precise location: ${err.message}`);
+      }
+    );
+  }, []);
+
+  return {
+    location,
+    error,
+    getLocation,
+  };
 };
+
+const convertLatLongToCityCountry = async (
+  latitude: number,
+  longitude: number
+) => {
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_OPENCAGE_API_KEY;
+    const response = await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`
+    );
+
+    if (response.data.results && response.data.results.length > 0) {
+      const components = response.data.results[0].components;
+      const city = components.city || components.town || components.village;
+      const country = components.country;
+
+      if (city && country) {
+        return { city, country };
+      } else {
+        throw new Error("City or country not found");
+      }
+    } else {
+      throw new Error("No results from geocoding API");
+    }
+  } catch (error) {
+    throw new Error("Error converting coordinates to location");
+  }
+};
+
 export default useBrowserGeolocation;
